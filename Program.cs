49da20cd1783ef3d35a2bc475fbe683eb257dea7
RahulAid <raccess97@gmail.com>
2022-11-13 20:24:01 +0530

    Max Number at Position 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Generics_Test_Cases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problems on Generics");
+
+            int output = MaxNumberCheck.MaxIntegerNumber(2222, 111, 11);
+            Console.WriteLine("\nMax integer number is: " + output);
+        }
+    }
+}
