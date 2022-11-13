7c9644431db4a75d1c9f4435d353232d2a719bdd
RahulAid <raccess97@gmail.com>
2022-11-13 21:55:17 +0530

    Maximum String Word

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace Generics_Test_Cases
         {
             Console.WriteLine("Problems on Generics");
 
-            float output = MaxNumberCheck.MaxFloatNumber(11f, 111f, 1111f);
-            Console.WriteLine("\nMax float number is: " + output);
+            string output = MaxNumberCheck.MaxStringWord("Apple", "Peach", "Banana");
+            Console.WriteLine("\nMax string word is: " + output);
         }
     }
 }
