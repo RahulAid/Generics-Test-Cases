5c2f1b481aca859016c4db49641d0f17d8f42df5
RahulAid <raccess97@gmail.com>
2022-11-13 22:07:54 +0530

    Max Using 4 Parameters

--- MaxNumberCheck.cs
+++ MaxNumberCheck.cs
@@ -6,18 +6,35 @@ using System.Threading.Tasks;
 
 namespace Generics_Test_Cases
 {
-    public class MaxNumberCheck
+    public class MaxNumberCheck<T> where T : IComparable
     {
-        public static String MaxStringWord(string val1, string val2, string val3)
-        {
-            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0)
-                return val1;
-            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0)
-                return val2;
-            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0)
-                return val3;
-            return val1;
+        public T val1, val2, val3, val4;
 
+        public MaxNumberCheck(T val1, T val2, T val3, T val4)
+        {
+            this.val1 = val1;
+            this.val2 = val2;
+            this.val3 = val3;
+            this.val4 = val4;
+        }
+
+        public static T MaxNumber(T val1, T val2, T val3, T val4)
+        {
+            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0 && val1.CompareTo(val4) > 0)
+                return val1;
+            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0 && val2.CompareTo(val4) > 0)
+                return val2;
+            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val2) > 0 && val3.CompareTo(val4) > 0)
+                return val3;
+            if (val4.CompareTo(val1) > 0 && val4.CompareTo(val2) > 0 && val4.CompareTo(val3) > 0)
+                return val4;
+            return val1;
+        }
+
+        public T MaxMethod()
+        {
+            T max = MaxNumberCheck<T>.MaxNumber(this.val1, this.val2, this.val3, this.val4);
+            return max;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,8 +7,14 @@ namespace Generics_Test_Cases
         {
             Console.WriteLine("Problems on Generics");
 
-            string output = MaxNumberCheck.MaxStringWord("Apple", "Peach", "Banana");
-            Console.WriteLine("\nMax string word is: " + output);
+            MaxNumberCheck<int> objint = new MaxNumberCheck<int>(11, 111, 1111, 22222);
+            Console.WriteLine("\nMax integer number is: " + objint.MaxMethod());
+
+            MaxNumberCheck<float> objfloat = new MaxNumberCheck<float>(1, 11, 111, 2222);
+            Console.WriteLine("\nMax float number is: " + objfloat.MaxMethod());
+
+            MaxNumberCheck<string> objstring = new MaxNumberCheck<string>("Apple", "Peach", "Banana", "Guava");
+            Console.WriteLine("\nMax string word is: " + objstring.MaxMethod());
         }
     }
 }
